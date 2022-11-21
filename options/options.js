ef321817df1a4641df015f1f541446fe3e126320
Crowdbotics <engineering@crowdbotics.com>
2022-11-21 05:42:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vita435-dev-45687",
+  url: "https://vita435-dev-45687.botics.co",
+  api: "https://vita435-dev-45687.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
